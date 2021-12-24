df2f46d5ba4719528cc4ced395bded6c3af23023
Agyemang, Eric <eagyem2@ilstu.edu>
2021-12-24 17:22:03 -0600

    Update Covid 19 data analysis.py

--- Code/Covid 19 data analysis.py
+++ Code/Covid 19 data analysis.py
@@ -3,12 +3,9 @@
 
 # In[1]:
 
-
 ### FITTING MULTI LINEAR REGRESSION MODEL FOR COVID DATASET
 
-
 # In[2]:
-
 
 ## Modules required
 import pandas as pd
@@ -18,9 +15,7 @@ import pylab
 import math
 import matplotlib.pyplot as plt
 
-
 # In[3]:
-
 
 from scipy import stats
 import statsmodels.api as sm
@@ -30,36 +25,26 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 get_ipython().run_line_magic('matplotlib', 'inline')
 
-
 # In[4]:
-
 
 ## Load the dataset into pandas
 covid19=pd.read_excel('covid19.xlsx')
 
-
 # In[5]:
-
 
 covid19.head()
 
-
 # In[6]:
-
 
 ## set the index equal to the year column
 covid19.index = covid19['CDHS']
 covid19 = covid19.drop(['STATE', 'STCD','REGION','CDHS'], axis = 1)
 
-
 # In[7]:
-
 
 covid19.head()
 
-
 # In[8]:
-
 
 ## Get the summary of our original data set
 desc_covid19 = covid19.describe()
@@ -68,16 +53,12 @@ desc_covid19.loc['+3_std']=desc_covid19.loc['mean']+(desc_covid19.loc['std']*3)
 desc_covid19.loc['-3_std']=desc_covid19.loc['mean']-(desc_covid19.loc['std']*3)
 desc_covid19
 
-
 # In[9]:
-
 
 ## Data preprocessing ##
 ## How is the distribution of the dependent variables?
 
-
 # In[10]:
-
 
 ## Condisder CNCS 
 CNCS = covid19.CNCS 
@@ -86,9 +67,7 @@ plt.show()
 stats.probplot(CNCS, dist="norm", plot=pylab)
 pylab.show()
 
-
 # In[11]:
-
 
 ## Performing data transformation on this variable for normality
 CNCS_bc, lmda = stats.boxcox(CNCS)
@@ -98,9 +77,7 @@ stats.probplot(CNCS_bc, dist = "norm", plot=pylab)
 pylab.show()
 print("lambda parameter for Box-Cox Transformation is {}".format(lmda))
 
-
 # In[12]:
-
 
 ## Condisder MRAT 
 MRAT = covid19.MRAT 
@@ -109,9 +86,7 @@ plt.show()
 stats.probplot(MRAT, dist="norm", plot=pylab)
 pylab.show()
 
-
 # In[13]:
-
 
 ## Performing data transformation on this variable for normality
 MRAT_bc, lmda = stats.boxcox(MRAT)
@@ -121,16 +96,12 @@ stats.probplot(MRAT_bc, dist = "norm", plot=pylab)
 pylab.show()
 print("lambda parameter for Box-Cox Transformation is {}".format(lmda))
 
-
 # In[14]:
-
 
 covid19["MRAT"] = MRAT_bc
 covid19["CNCS"] = CNCS_bc
 
-
 # In[15]:
-
 
 ## Checking the Model Assumptions
 ######## Multicolinearity #################
@@ -139,16 +110,12 @@ corr=covid19.corr()
 ## Display the correlation matrix
 display(corr)
 
-
 # In[16]:
-
 
 ## plot a heatmap
 sns.heatmap(corr, xticklabels = corr.columns, yticklabels = corr.columns, cmap="RdBu")
 
-
 # In[52]:
-
 
 ### Using the VIF to measure to detect the above and dropping all variable with greater than 10 VIF
 covid19_before = covid19
@@ -169,15 +136,11 @@ print('DATA AFTER')
 print('-'*100)
 display(series_after)
 
-
 # In[53]:
-
 
 covid19_after
 
-
 # In[54]:
-
 
 #### Building the model ####
 ## considering CNCS as our dependent Variable ##
@@ -185,9 +148,7 @@ covid19_after
 x = covid19_after.drop(['CNCS', 'MRAT'], axis = 1)
 y = covid19_after['CNCS']
 
-
 # In[55]:
-
 
 ## Split dataset into training and testing portion
 from sklearn.model_selection import train_test_split
@@ -204,16 +165,12 @@ min_max_scaler= preprocessing.MinMaxScaler()
 x_train_minmax = min_max_scaler.fit_transform(x_train)
 x_test_minmax = min_max_scaler.fit_transform(x_test)
 
-
 # In[56]:
-
 
 x_train = x_train_minmax
 x_test= x_test_minmax
 
-
 # In[57]:
-
 
 ## Create an instance of our model
 regression_model = LinearRegression()
@@ -224,15 +181,12 @@ regression_model.fit(x_train, y_train)
 
 # In[58]:
 
-
 ## Getting multiple prediction
 y_predict = regression_model.predict(x_test)
 ## Show the first five
 y_predict[:5]
 
-
 # In[59]:
-
 
 ## Evaluating the model
 import statsmodels.api as sm
@@ -247,9 +201,7 @@ model = sm.OLS(y, x2)
 ## fit the data
 est = model.fit()
 
-
 # In[60]:
-
 
 ## Testing the Model Assumptions
 # Heteroscedasticity using the Breusch-Pegan test
@@ -269,9 +221,7 @@ else:
     print("The p-value was {:.4}".format(pval))
     print("we reject the null hypothesis, and conclude that there is heteroscedasticity.")
 
-
 # In[61]:
-
 
 ### Checking for Autocorrelation using the Ljungbox test
 #H0: The data are random
@@ -307,9 +257,7 @@ from statsmodels.stats import diagnostic as diag
 sm.graphics.tsa.plot_acf(est.resid)
 plt.show()
 
-
 # In[62]:
-
 
 ## Check for Linearity of the residuals using the Q-Q plot
 import pylab
@@ -320,28 +268,10 @@ pylab.show()
 mean_residuals = sum(est.resid)/len(est.resid)
 mean_residuals
 
-
 # In[63]:
-
 
 ## Model summary
 print(est.summary())
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[64]:
-
 
 #### Building the model ####
 ## considering MRAT as our dependent Variable ##
@@ -349,9 +279,7 @@ print(est.summary())
 x = covid19_after.drop(['CNCS', 'MRAT'], axis = 1)
 y = covid19_after['MRAT']
 
-
 # In[65]:
-
 
 ## Split dataset into training and testing portion
 from sklearn.model_selection import train_test_split
@@ -368,16 +296,12 @@ min_max_scaler= preprocessing.MinMaxScaler()
 x_train_minmax = min_max_scaler.fit_transform(x_train)
 x_test_minmax = min_max_scaler.fit_transform(x_test)
 
-
 # In[66]:
-
 
 x_train = x_train_minmax
 x_test= x_test_minmax
 
-
 # In[67]:
-
 
 ## Create an instance of our model
 regression_model = LinearRegression()
@@ -385,18 +309,14 @@ regression_model = LinearRegression()
 ## Fit the model
 regression_model.fit(x_train, y_train)
 
-
 # In[68]:
-
 
 ## Getting multiple prediction
 y_predict = regression_model.predict(x_test)
 ## Show the first five
 y_predict[:5]
 
-
 # In[69]:
-
 
 ## Evaluating the model
 import statsmodels.api as sm
@@ -411,9 +331,7 @@ model = sm.OLS(y, x2)
 ## fit the data
 est = model.fit()
 
-
 # In[70]:
-
 
 ## Testing the Model Assumptions
 # Heteroscedasticity using the Breusch-Pegan test
@@ -433,9 +351,7 @@ else:
     print("The p-value was {:.4}".format(pval))
     print("we reject the null hypothesis, and conclude that there is heteroscedasticity.")
 
-
 # In[71]:
-
 
 ### Checking for Autocorrelation using the Ljungbox test
 #H0: The data are random
@@ -471,9 +387,7 @@ from statsmodels.stats import diagnostic as diag
 sm.graphics.tsa.plot_acf(est.resid)
 plt.show()
 
-
 # In[72]:
-
 
 ## Check for Linearity of the residuals using the Q-Q plot
 import pylab
@@ -484,22 +398,10 @@ pylab.show()
 mean_residuals = sum(est.resid)/len(est.resid)
 mean_residuals
 
-
 # In[73]:
-
 
 ## Model summary
 print(est.summary())
 
 
 # In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
